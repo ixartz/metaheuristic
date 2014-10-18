c159a3972cdbd9ab0284fc41cae3d510caa1a846
Rémi Weng <ixartz@users.noreply.github.com>
2014-10-18 16:57:52 +0200

    Create OpenGL window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,35 @@
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main()
 {
-    return 0;
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    window = glfwCreateWindow(640, 320, "Metaheuristic", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
